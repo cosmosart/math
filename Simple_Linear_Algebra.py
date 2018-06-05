227a4edc82cd0b9a558b1ec3036aca52efa1a727
Eli Lee Kitagawa <cosmosart@msn.com>
2018-06-05 14:26:07 +0900

    Update Simple_Linear_Algebra.py

--- Simple_Linear_Algebra.py
+++ Simple_Linear_Algebra.py
@@ -3,6 +3,6 @@ def interest_calculate(amount, rate, year, compound = 1, continus = False):
     if continus == False:
         return round(amount * (1 + (rate/100)/compound)**(year*compound),2)
     elif continus == True:
-        return round(amount * math.exp((rate/100)*year))
+        return round(amount * math.exp((rate/100)*year),2)
     else:
         print('Usage "interest_calculate(amount, rate, year, compound(option), continus(option))"')
