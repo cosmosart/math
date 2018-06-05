db842410a4019c49eb09dada852e332c4a430ae6
cosmosart <cosmosart@msn.com>
2018-06-05 15:22:40 +0900

    Synthetic_division

---
+++ Remainder_Theorem.py
@@ -0,0 +1,9 @@
+def synthetic_division(x,equation_coff):
+    output = []
+    division = 0
+    for i in range(len(equation_coff)):
+        y = equation_coff[i] + division
+        output.append(y)
+        division = x * y
+    
+    return(output[:-1], output[-1])
