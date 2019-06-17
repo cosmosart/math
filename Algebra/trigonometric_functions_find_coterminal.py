e88cad24846be7349353abeea4d263bc195bceb8
Eli Kitagawa <cosmosart@msn.com>
2019-06-18 06:49:42 +0900

    add trigonometric func

---
+++ Algebra/trigonometric_functions_find_coterminal.py
@@ -0,0 +1,8 @@
+def find_coterminal(angle,type="degree"):
+    if type == "degree":
+        return angle % 360
+    elif type == "radian":
+#        import math
+#        return angle % math.pi * 2
+        print("Not yet")
+        
