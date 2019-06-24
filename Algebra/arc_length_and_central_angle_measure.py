bb83463ebc55c018e1245f95b4d6402313954250
Eli Kitagawa <cosmosart@msn.com>
2019-06-24 17:43:09 +0900

    fix

--- Algebra/arc_length_and_central_angle_measure.py
+++
@@ -1,7 +0,0 @@
-def length_of_arc_function(ra,angle,type="degree"):
-    import math
-    if type == "degree":
-        return ra*angle*math.pi/180
-    elif type == "radians":
-        return ra*angle
-
